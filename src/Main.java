2e2e0f885dfdeeba20adef99babf0b144d5ad98e
Hashlama95 <nikita.9a@mail.ru>
2022-09-16 14:56:11 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+       /* int i = 0;
+        for (; i < 3; ) {
+
+        }
+        while (i < 3) {
+        }
+        do {
+        } while (i<3);*/
+
+        /*int[] arr={2,0,1,3};
+        for (int i=0; i<arr.length; i++) {
+            int el = arr[i];
+            System.out.println(el);
+        }*/
+
+        int i;
+        for (i=0;i<237;i+=10){
+            System.out.println(i);
+        }
+    }
+}
